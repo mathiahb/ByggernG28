1300521802e23e1f68344b995829c0a5b75e6372
Mathias H Bæverfjord <mathiahb@stud.ntnu.no>
2024-09-12 15:44:46 +0200

    SRAM_TEST

--- main.c
+++ main.c
@@ -4,6 +4,44 @@
 
 #include "stdio.h"
 
+#ifdef SRAM_TEST
+#include "stdint.h"
+#include <stdlib.h>
+void SRAM_test(void){
+  volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
+      uint16_t ext_ram_size = 0x800;
+      uint16_t write_errors = 0;
+      uint16_t retrieval_errors = 0;
+      printf("Starting SRAM test...\r\n");
+      // rand() stores some internal state, so calling this function in a loop will
+      // yield different seeds each time (unless srand() is called before this function)
+      uint16_t seed = rand();
+      // Write phase: Immediately check that the correct value was stored
+      srand(seed);
+      for (uint16_t i = 0; i < ext_ram_size; i++) {
+          uint8_t some_value = rand();
+          ext_ram[i] = some_value;
+          uint8_t retreived_value = ext_ram[i];
+          if (retreived_value != some_value) {
+              printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\r\n", i, retreived_value, some_value);
+              write_errors++;
+          }
+      }
+      // Retrieval phase: Check that no values were changed during or after the write phase
+      srand(seed);
+      // reset the PRNG to the state it had before the write phase
+      for (uint16_t i = 0; i < ext_ram_size; i++) {
+          uint8_t some_value = rand();
+          uint8_t retreived_value = ext_ram[i];
+          if (retreived_value != some_value) {
+              printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\r\n", i, retreived_value, some_value);
+              retrieval_errors++;
+          }
+      }
+      printf("SRAM test completed with \r\n%4d errors in write phase and \r\n%4d errors in retrieval phase\r\n\r\n", write_errors, retrieval_errors);
+}
+#endif
+
 int main()
 {
   init_uart();
@@ -14,18 +52,6 @@ int main()
 
   while (1)
   {
-    /*if (uart_unread_data_in_buffer() && uart_ready_to_send())
-    {
-      char letter = receive_uart();
-      send_uart(letter);
-      write_pin(A, 2, on);
-    }
-    else if (uart_ready_to_send() && !sleep)
-    {
-      send_uart('a');
-      write_pin(A, 1, on);
-    }*/
-
     if((sleep % 5000) == 0){
         printf("Sleep: %d\n", sleep);
     }
@@ -33,12 +59,10 @@ int main()
     if (!sleep)
     {
       sleep = 10000;
-      
-      printf("Latch pin %d\n", latch_pin);
 
-      XMEM[(1 << latch_pin)] = 0;
-      latch_pin++;
-      latch_pin %= 8;
+#ifdef SRAM_TEST
+      SRAM_test();
+#endif
     }
 
     --sleep;
